b2ffc7a71017071427bf56018b49e4530bdd6996
DESKTOP-LRKIFMR\UI1 <209315540@qq.com>
2023-03-28 18:03:31 +0800

    feat(article item): 更新文章单项显示逻辑

--- src/components/ArticleItem/index.tsx
+++ src/components/ArticleItem/index.tsx
@@ -7,19 +7,22 @@ const { Title, Paragraph } = Typography;
 const ListItem: React.FC<any> = ({ item }) => {
   return (
     <div className="article-item">
-      <Title>{ item.article_info?.title }</Title>
+      <Title level={4}>{ item.article_info?.title }</Title>
       <div className="article-item__info">
         <span>{ item.author_user_info?.user_name }</span>
         <span>{ item.article_info?.ctime }</span>
       </div>
       <Row>
-        <Col>
-          <Image
-            width={200}
-            src={item.article_info?.cover_image}
-          />
-        </Col>
-        <Col>
+        {
+          item.article_info?.cover_image &&
+          <Col span={4}>
+            <Image
+              width={200}
+              src={item.article_info?.cover_image}
+            />
+          </Col>
+        }
+        <Col span={item.article_info?.cover_image ? 20 : 24}>
           <Paragraph>
             { item.article_info?.brief_content }
           </Paragraph>
