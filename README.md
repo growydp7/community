b0bccbc8068c9a648172e428d494b7419058e226
DESKTOP-LRKIFMR\UI1 <209315540@qq.com>
2023-03-28 17:15:49 +0800

    feat(project): 更新项目依赖

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ## 更新日志
 
 [CHANGELOG]('CHANGELOG.md')
+
+## TODO
+
+- 项目config
+- 项目路由
+- 项目状态管理
+
+## Tools
+
+- [react-router-dom](https://reactrouter.com/en/main)
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,36 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: "antd",
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    })
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
     transformMode: {
       web: [/.[tj]sx$/]
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          '@primary-color': '#4377FE',//设置antd主题色
+        },
+      },
     }
   }
 })
